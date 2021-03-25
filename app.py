683699f27541afe2dafa3582eedd5022eb8653df
Emelian Nichiforv <pau4ok121@gmail.com>
2021-03-25 05:35:32 +0300

    I have a problem

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
+git
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_SRI"]="sqlite:///test.db"
+db = SQLAlchemy(app)
 
 
 @app.route("/", methods=['GET', 'POST'])
