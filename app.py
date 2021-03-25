f5ef3d4cb5cf9064e6d4ccf6084e41b078ee2a6b
Emelian Nichiforv <pau4ok121@gmail.com>
2021-03-26 00:14:51 +0300

    Tried to make it work

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-git
+from flask_wtf import FlaskForm
+from wtforms import FileField
+from flask_uploads import configure_uploads, IMAGES, UploadSet
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_SRI"]="sqlite:///test.db"
-db = SQLAlchemy(app)
+app.config["SECRET_KEY"] = "secret"
+app.config["UPLOADED_IMAGES_DEST"] = "uploads/images"
 
+images = UploadSet("images", IMAGES)
+configure_uploads(app, images)
 
-@app.route("/", methods=['GET', 'POST'])
+class MyForm(FlaskForm):
+    image = FileField("image")
+
+@app.route('/', methods=["GET","POST"])
 def index():
-    return render_template("index.html")
+    form = MyForm()
 
+    if form.validate_on_submit():
+
+        filename = images.save(form.image.data)
+        return ("Filename: {}".format(filename))
+
+    return render_template("index.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
